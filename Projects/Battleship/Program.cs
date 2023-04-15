0691ed98ab6296764e329b150c861fead738483d
WorkPC <smirkstick@gmail.com>
2023-04-15 12:34:59 +1200

    Moved ship enum into own file and made public

--- Projects/Battleship/Program.cs
+++ Projects/Battleship/Program.cs
@@ -486,12 +486,3 @@ public static class Program
 		};
 	}
 }
-
-internal enum Ship
-{
-	[Tag("size", 5)] Carrier = 1,
-	[Tag("size", 4)] Battleship = 2,
-	[Tag("size", 3)] Cruiser = 3,
-	[Tag("size", 3)] Submarine = 4,
-	[Tag("size", 2)] Destroyer = 5,
-}
---
+++ Projects/Battleship/Ship.cs
@@ -0,0 +1,10 @@
+﻿using Towel;
+
+public enum Ship
+{
+	[Tag("size", 5)] Carrier = 1,
+	[Tag("size", 4)] Battleship = 2,
+	[Tag("size", 3)] Cruiser = 3,
+	[Tag("size", 3)] Submarine = 4,
+	[Tag("size", 2)] Destroyer = 5,
+}
